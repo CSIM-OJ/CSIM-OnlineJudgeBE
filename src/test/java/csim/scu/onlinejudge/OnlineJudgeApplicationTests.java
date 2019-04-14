c5638fe497d194690e316d71d056407776c10d21
KennyChenFight <kennychen851228@gmail.com>
2019-04-14 21:45:17 +0800

    封裝Web環境及轉json的method

--- src/test/java/csim/scu/onlinejudge/OnlineJudgeApplicationTests.java
+++ src/test/java/csim/scu/onlinejudge/OnlineJudgeApplicationTests.java
@@ -5,13 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 
@@ -22,9 +25,11 @@ public class OnlineJudgeApplicationTests {
 
 	@Autowired
 	private WebApplicationContext context;
-	MockMvc mvc;
+	protected MockMvc mvc;
+    protected HttpSession session;
+
 	@Autowired
-	ObjectMapper objectMapper;
+	public ObjectMapper objectMapper;
 
 	@Before
 	public void setUp() {
