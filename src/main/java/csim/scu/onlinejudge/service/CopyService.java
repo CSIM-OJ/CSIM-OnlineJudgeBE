3823b40e26d045d2fef19567f51e98a53c78f5e9
KennyChenFight <kennychen851228@gmail.com>
2019-04-16 01:34:24 +0800

    實踐findByStudentTwoAccount、findByProblem

--- src/main/java/csim/scu/onlinejudge/service/CopyService.java
+++ src/main/java/csim/scu/onlinejudge/service/CopyService.java
@@ -1,10 +1,14 @@
 package csim.scu.onlinejudge.service;
 
 import csim.scu.onlinejudge.dao.domain.copy.Copy;
+import csim.scu.onlinejudge.dao.domain.problem.Problem;
 import csim.scu.onlinejudge.service.base.BaseService;
 
 import java.util.List;
 
 public interface CopyService extends BaseService<Copy, Long> {
 
+    List<Copy> findByStudentTwoAccount(String account);
+
+    List<Copy> findByProblem(Problem problem);
 }
--- src/main/java/csim/scu/onlinejudge/service/impl/CopyServiceImpl.java
+++ src/main/java/csim/scu/onlinejudge/service/impl/CopyServiceImpl.java
@@ -1,6 +1,7 @@
 package csim.scu.onlinejudge.service.impl;
 
 import csim.scu.onlinejudge.dao.domain.copy.Copy;
+import csim.scu.onlinejudge.dao.domain.problem.Problem;
 import csim.scu.onlinejudge.dao.repository.CopyRepository;
 import csim.scu.onlinejudge.dao.repository.base.BaseRepository;
 import csim.scu.onlinejudge.service.CopyService;
@@ -25,4 +26,13 @@ public class CopyServiceImpl extends BaseServiceImpl<Copy, Long> implements Copy
         return copyRepository;
     }
 
+    @Override
+    public List<Copy> findByStudentTwoAccount(String account) {
+        return copyRepository.findByStudentTwoAccount(account);
+    }
+
+    @Override
+    public List<Copy> findByProblem(Problem problem) {
+        return copyRepository.findByProblem(problem);
+    }
 }
