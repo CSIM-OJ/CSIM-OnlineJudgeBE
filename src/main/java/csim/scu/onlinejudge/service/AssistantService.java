7ca8ca40c8c66d32ee65027c1e9004d0a49ea11b
KennyChenFight <kennychen851228@gmail.com>
2019-04-30 10:36:43 +0800

    新增getAssistantListInfo

--- src/main/java/csim/scu/onlinejudge/service/AssistantService.java
+++ src/main/java/csim/scu/onlinejudge/service/AssistantService.java
@@ -2,7 +2,10 @@ package csim.scu.onlinejudge.service;
 
 import csim.scu.onlinejudge.common.exception.EntityNotFoundException;
 import csim.scu.onlinejudge.dao.domain.assistant.Assistant;
+import csim.scu.onlinejudge.dao.domain.assistant.AssistantInfo;
 import csim.scu.onlinejudge.service.base.BaseService;
+
+import java.util.List;
 
 public interface AssistantService extends BaseService<Assistant, Long> {
 
@@ -11,4 +14,6 @@ public interface AssistantService extends BaseService<Assistant, Long> {
     Assistant findByAccount(String account) throws EntityNotFoundException;
 
     int updatePasswordByAccount(String account, String oriPassword, String newPassword);
+
+    List<AssistantInfo> getAssistantListInfo();
 }
