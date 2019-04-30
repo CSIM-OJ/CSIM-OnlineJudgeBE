ffb1f427ddfa627a94520f8b80081561462f63b6
KennyChenFight <kennychen851228@gmail.com>
2019-04-30 10:31:42 +0800

    新增 getStudentsData

--- src/main/java/csim/scu/onlinejudge/manager/ProblemManager.java
+++ src/main/java/csim/scu/onlinejudge/manager/ProblemManager.java
@@ -8,4 +8,6 @@ import java.util.Map;
 public interface ProblemManager {
 
     List<Map<String, String>> getStudentProblemInfo(Long courseId, String type, boolean isJudge, String account) throws EntityNotFoundException;
+
+    List<Map<String, Object>> getStudentsData(Long courseId) throws EntityNotFoundException;
 }
