c82fc47daad8ab6e3ee0d0306de2c528ded0d5af
KennyChenFight <kennychen851228@gmail.com>
2019-05-19 17:38:30 +0800

    不遮住最後範本，由前端控制

--- src/main/java/csim/scu/onlinejudge/service/impl/ProblemServiceImpl.java
+++ src/main/java/csim/scu/onlinejudge/service/impl/ProblemServiceImpl.java
@@ -59,9 +59,7 @@ public class ProblemServiceImpl extends BaseServiceImpl<Problem, Long> implement
     public ProblemInfo getInfo(Long problemId) throws EntityNotFoundException {
         Optional<Problem> instance = problemRepository.findById(problemId);
         Problem problem = instance.orElseThrow(EntityNotFoundException::new);
-        // 最後一個testcase為隱藏範本，故遮掉
         List<TestCase> testCases = problem.getTestCases();
-        testCases.remove(testCases.size() - 1);
         ProblemInfo problemInfo = new ProblemInfo(problem.getName(),
                 problem.getType(), problem.getCategory(), problem.getTag(),
                 problem.getRate(), problem.getDescription(),
